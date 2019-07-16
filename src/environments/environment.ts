4676a3c361bc7cb4879e91668fe1ec65f053ec6a
jkyony <jkyony@gmail.com>
2019-07-16 16:21:28 +0200

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false , 
+  firebase: {
+    apiKey: "AIzaSyCpWAgboJusJqS9EUCRXZc4V7T_DGtH4Q0",
+  authDomain: "ichef-14324.firebaseapp.com",
+  databaseURL: "https://ichef-14324.firebaseio.com",
+  projectId: "ichef-14324",
+  storageBucket: "ichef-14324.appspot.com",
+  messagingSenderId: "629470272511",
+  appId: "1:629470272511:web:68226d0573f15547"
+
+  }
 };
 
 /*
