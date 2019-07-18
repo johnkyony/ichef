fdfd0124fa1abb399365053af2886e26eae84051
jkyony <jkyony@gmail.com>
2019-07-18 13:41:30 +0200

    pages added , new service added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'recipes-list', loadChildren: './pages/recipes-list/recipes-list.module#RecipesListPageModule' },
+  { path: 'recipes-details', loadChildren: './pages/recipes-details/recipes-details.module#RecipesDetailsPageModule' },
 ];
 
 @NgModule({
